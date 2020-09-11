fe7b35188b03bbdc52179bf6b80efeae78a05397
lzxue <lzx199065@gmail.com>
2020-09-11 10:05:46 +0800

    chore: remove lib es files

--- es/component/LayerContext.js
+++
@@ -1,6 +0,0 @@
-import { createContext, useContext } from 'react'; // tslint:disable-next-line: no-object-literal-type-assertion
-
-export var LayerContext = createContext({});
-export function useLayerValue() {
-  return useContext(LayerContext);
-}
--- es/component/SceneContext.js
+++
@@ -1,6 +0,0 @@
-import { createContext, useContext } from 'react'; // tslint:disable-next-line: no-object-literal-type-assertion
-
-export var SceneContext = createContext({});
-export function useSceneValue() {
-  return useContext(SceneContext);
-}
