b619dbbf8d3340ea8f0852b7301472abab7c3562
象数 <zhengxue.lzx@antgroup.com>
2021-09-27 10:58:09 +0800

    fix: 接口定义导出

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export * from './component/SceneContext';
-export { default as AMapScene } from './component/AMapScene';
-export { default as MapboxScene } from './component/MapboxScene';
-export { default as AMapSceneV2 } from './component/AMapSceneV2';
-export { default as MapScene } from './component/MapScene';
+export { default as AMapScene } from './component/MapScene/AMapScene';
+export { default as MapboxScene } from './component/MapScene/MapboxScene';
+export { default as AMapSceneV2 } from './component/MapScene/AMapSceneV2';
+export { default as MapScene } from './component/MapScene/MapScene';
 export { default as Scene } from './component/Scene';
 export { default as Control } from './component/Control';
 export { default as CustomControl } from './component/CustomControl';
